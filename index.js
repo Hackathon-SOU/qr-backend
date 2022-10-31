4368b5fc3f9c309bd9286dbdd32812b7eee090c1
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-10-31 15:19:34 +0530

    git initalize and project created

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const mongoose= require('mongoose');
+const cors= require('cors');
+const path = require('path');
+require("dotenv").config();
+
+
+const mongodbString= process.env.DATABASE_URL;
+mongoose.connect(mongodbString);
+const database = mongoose.connection;
+
+database.on("error", (error) =>{
+    console.log("Database is not connected", error);
+});
+
+database.once("connected", () =>{
+    console.log("Database is connected");
+});
+
+
+
+const app= express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use("/api", require("./routes/login"));
+app.use("/api/attendance", require("./routes/attendance"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.port || 4000, () =>{
+    console.log("Server is running on port 4000");
+});
