07846b112fcd4d407b1a4431a3ba8a3d42180d3d
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-11-30 22:22:44 +0530

    test multer

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const options = {
     customSiteTitle: "Qr Server API - Swagger"
 };
 
+app.use('/uploads', express.static(path.join(ROOT_FOLDER, 'uploads')));
 app.use('/api/api-docs/swagger-ui.css', express.static(path.join(ROOT_FOLDER, 'public/swagger-ui.css')));
 app.use('/api/api-docs', swaggerUi.serve);
 app.use('/api/api-docs', swaggerUi.setup(swaggerDocument, options));
