37ac0a18c6cfa11e61b8c14bb44f3fc90fc2cbbf
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-12-08 18:18:50 +0530

    changed folder path and to upload only xlsx files only

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,14 +1,16 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const httpStatus = require("http-status");
 
+const ApiError = require("../utils/ApiError")
 const logger = require("../utils/logger");
 
 const multerUpload = async (req, res, next) => {
     let fileName = "";
     let storage = multer.diskStorage({
         destination: function (req, file, callback) {
-            console.log(path.join(path.resolve(), "./uploads/"));
+            console.log(path.join(path.resolve(), "src/uploads/"));
             fs.mkdir('./uploads/', (err) => {
                 callback(null, path.join(path.resolve(), "./uploads/"));
             });
@@ -22,13 +24,18 @@ const multerUpload = async (req, res, next) => {
 
     // below code is to read the added data to DB from file
     var upload = multer({
-        storage: storage
+        storage: storage,
+        fileFilter: function (req, file, callback) {
+            var ext = path.extname(file.originalname);
+            if (ext !== '.xlsx') {
+                return callback(new Error('Only Excel sheets are allowed'))
+            }
+            callback(null, true)
+        },
     }).single("sheet");
     upload(req, res, async function (err) {
         if (err) {
-            return res.status(500).send({
-                message: err
-            });
+            next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, err.message));
         } else {
             req.fileName = fileName;
             next();
