9a65e4d2ac0a1f1392586850f1e00232df1d782d
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2022-12-30 15:57:43 +0530

    upload sheet error responses fix

--- src/controllers/userDetailsController.js
+++ src/controllers/userDetailsController.js
@@ -192,7 +192,6 @@ const uploadSheet = async (req, res, next) => {
                     documentCount = result.deletedCount;
                     logger.info("%s totalCount", totalData);
                     logger.info("sheetLength ===> %s", sheetData.length);
-                    let errorArray = [];
                     sheetData.forEach(async (a) => {
                         const data = new userData({
                             name: a.name,
@@ -206,20 +205,14 @@ const uploadSheet = async (req, res, next) => {
                             if (error) {
                                 if (error.code == 11000) {
                                     if (error.keyPattern.regId) {
-                                        errorArray.push({
-                                            error: `Duplicate ${a.regId} regId for participant ${a.name} in sheet`,
-                                        });
                                         logger.error("uploadSheet, Duplicate regId in sheet, ==> %o", error);
+                                        next(new ApiError(httpStatus.CONFLICT, `Duplicate ${a.regId} regId for participant ${a.name} in sheet`));
                                     } else if (error.keyPattern.email) {
-                                        errorArray.push({
-                                            error: `Duplicate ${a.email} email for participant ${a.name} in sheet`,
-                                        });
                                         logger.error("uploadSheet, Duplicate email in sheet==> %o", error);
+                                        next(new ApiError(httpStatus.CONFLICT, `Duplicate ${a.email} email for participant ${a.name} in sheet`));
                                     } else if (error.keyPattern.seatNo) {
-                                        errorArray.push({
-                                            error: `Seat no ${a.seatNo} can not allocate to particpant ${a.name} in sheet`,
-                                        });
                                         logger.error("uploadSheet, Duplication seatNo in sheet==> %o", error);
+                                        next(new ApiError(httpStatus.CONFLICT, `Seat no ${a.seatNo} can not allocate to particpant ${a.name} in sheet`));
 
                                     }
                                 }
