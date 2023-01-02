0158867083b2806439466eccc312544d620e9df6
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2023-01-02 22:26:47 +0530

    vercel mail send test

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -20,14 +20,30 @@ let mailOptions = {
 
 const sendRegisterAdminMail = async (adminMail, membershipId, password) => {
     try {
+        await new Promise((resolve, reject) => {
+            // verify connection configuration
+            transporter.verify(function (error, success) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
+        });
         mailOptions.to = adminMail;
         mailOptions.text = `Your Account for IEEE Qr App has been successfully created. Your MembershipId is ${membershipId} and password is ${password}`
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                logger.error(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
+        await new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    logger.error(error);
+                    reject(err);
+                } else {
+                    logger.debug('Email sent: ' + info.response);
+                    resolve(info);
+                }
+            });
         });
     } catch (error) {
         logger.error(error)
