646fb10576964bb66e9e6dcafbb94a472f03581d
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2023-01-02 22:04:54 +0530

    logger imported in sendmail services

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
 require("dotenv").config();
+const logger = require("../utils/logger");
+
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
