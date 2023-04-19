65ccd768ea128af5f28ba80b7cfac3d779cc437a
bhanudattsinhjhala <jhalabhanudattsinh4090@gmail.com>
2023-04-19 14:09:26 +0530

    cors any origin for verify api

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,84 +1,153 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 const {
-    createEvent,
-    getEvent,
-    getEventReport,
-    deleteEvent
-} = require("../controllers/eventController")
+  createEvent,
+  getEvent,
+  getEventReport,
+  deleteEvent,
+} = require("../controllers/eventController");
 const {
-    getuserDetails,
-    getAllUserDetails,
-    uploadSheet,
-    markpresence,
-    singleUserData,
-    totalAbsent
-} = require("../controllers/userDetailsController")
+  getuserDetails,
+  getAllUserDetails,
+  uploadSheet,
+  markpresence,
+  singleUserData,
+  totalAbsent,
+} = require("../controllers/userDetailsController");
 const {
-    adminRegister,
-    verifyEmail,
-    canteenRegister,
-    adminLogin,
-    getAdminJwtToken
-} = require("../controllers/loginController")
+  adminRegister,
+  verifyEmail,
+  canteenRegister,
+  adminLogin,
+  getAdminJwtToken,
+} = require("../controllers/loginController");
 const authValidation = require("../validation/auth.validation.js");
 const participantValidation = require("../validation/participant.validation.js");
 const eventValidation = require("../validation/event.validation.js");
 const canteenValidation = require("../validation/canteen.validation.js");
-const {
-    getMenu,
-    getCanteen
-} = require("../controllers/canteenController")
-const validate = require("../middleware/validation.js")
-const {
-    verifyJwt,
-    authorizeAdmin
-} = require("../middleware/verifyJwt");
+const { getMenu, getCanteen } = require("../controllers/canteenController");
+const validate = require("../middleware/validation.js");
+const { verifyJwt, authorizeAdmin } = require("../middleware/verifyJwt");
 const multerUpload = require("../middleware/multer");
 const {
-    getAllMemberDetails,
-    deleteMemberAccount
-} = require('../controllers/memberController');
-
+  getAllMemberDetails,
+  deleteMemberAccount,
+} = require("../controllers/memberController");
 
 const router = express.Router();
 
-router.post('/signup', verifyJwt, validate(authValidation.adminRegisterSchema), adminRegister);
+router.post(
+  "/signup",
+  verifyJwt,
+  validate(authValidation.adminRegisterSchema),
+  adminRegister
+);
 
-router.get('/verify/:userId/:token', validate(authValidation.adminVerifySchema), verifyEmail);
+router.get(
+  "/verify/:userId/:token",
+  cors(),
+  validate(authValidation.adminVerifySchema),
+  verifyEmail
+);
 
-router.post('/login', validate(authValidation.adminLoginSchema), adminLogin);
+router.post("/login", validate(authValidation.adminLoginSchema), adminLogin);
 
-router.get('/getjwttoken', getAdminJwtToken);
+router.get("/getjwttoken", getAdminJwtToken);
 
-router.get('/getallmemberdetails', verifyJwt, authorizeAdmin, getAllMemberDetails);
+router.get(
+  "/getallmemberdetails",
+  verifyJwt,
+  authorizeAdmin,
+  getAllMemberDetails
+);
 
-router.delete('/deletemember', verifyJwt, authorizeAdmin, deleteMemberAccount);
+router.delete("/deletemember", verifyJwt, authorizeAdmin, deleteMemberAccount);
 
-router.post('/uploadSheet', verifyJwt, authorizeAdmin, multerUpload, uploadSheet);
+router.post(
+  "/uploadSheet",
+  verifyJwt,
+  authorizeAdmin,
+  multerUpload,
+  uploadSheet
+);
 
-router.get('/getalluserdetails', verifyJwt, authorizeAdmin, validate(participantValidation.getAllUserDetailsSchema), getAllUserDetails);
+router.get(
+  "/getalluserdetails",
+  verifyJwt,
+  authorizeAdmin,
+  validate(participantValidation.getAllUserDetailsSchema),
+  getAllUserDetails
+);
 
-router.get('/getuserdetails', verifyJwt, authorizeAdmin, validate(participantValidation.getUserDetailsSchema), getuserDetails);
+router.get(
+  "/getuserdetails",
+  verifyJwt,
+  authorizeAdmin,
+  validate(participantValidation.getUserDetailsSchema),
+  getuserDetails
+);
 
-router.post('/singleuserdata', verifyJwt, validate(authValidation.userRegisterSchema), authorizeAdmin, singleUserData);
+router.post(
+  "/singleuserdata",
+  verifyJwt,
+  validate(authValidation.userRegisterSchema),
+  authorizeAdmin,
+  singleUserData
+);
 
-router.put('/markpresence', verifyJwt, authorizeAdmin, validate(participantValidation.markPresenceSchema), markpresence);
+router.put(
+  "/markpresence",
+  verifyJwt,
+  authorizeAdmin,
+  validate(participantValidation.markPresenceSchema),
+  markpresence
+);
 
-router.get('/geteventreport', verifyJwt, authorizeAdmin, validate(eventValidation.eventReportSchema), getEventReport);
+router.get(
+  "/geteventreport",
+  verifyJwt,
+  authorizeAdmin,
+  validate(eventValidation.eventReportSchema),
+  getEventReport
+);
 
-router.delete('/deleteevent', verifyJwt, authorizeAdmin, validate(eventValidation.eventDeleteSchema), deleteEvent);
+router.delete(
+  "/deleteevent",
+  verifyJwt,
+  authorizeAdmin,
+  validate(eventValidation.eventDeleteSchema),
+  deleteEvent
+);
 
-router.get('/totalabsent', verifyJwt, authorizeAdmin, totalAbsent);
+router.get("/totalabsent", verifyJwt, authorizeAdmin, totalAbsent);
 
-router.post("/createevent", verifyJwt, authorizeAdmin, validate(authValidation.eventRegisterSchema), createEvent);
+router.post(
+  "/createevent",
+  verifyJwt,
+  authorizeAdmin,
+  validate(authValidation.eventRegisterSchema),
+  createEvent
+);
 
 router.get("/getevent", verifyJwt, authorizeAdmin, getEvent);
 
 router.get("/getcanteen", verifyJwt, authorizeAdmin, getCanteen);
 
-router.post("/createcanteen", verifyJwt, validate(authValidation.canteenRegisterSchema), authorizeAdmin, canteenRegister);
+router.post(
+  "/createcanteen",
+  verifyJwt,
+  validate(authValidation.canteenRegisterSchema),
+  authorizeAdmin,
+  canteenRegister
+);
 
-router.get("/getmenu", verifyJwt, authorizeAdmin, validate(canteenValidation.getMenuSchema), getMenu);
+router.get(
+  "/getmenu",
+  verifyJwt,
+  authorizeAdmin,
+  validate(canteenValidation.getMenuSchema),
+  getMenu
+);
 
 module.exports = router;
